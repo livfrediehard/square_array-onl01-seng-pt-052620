38bf7d1e198d3f8dbc07d3a678754f16ff09ec81
Brian Palmer <bpalmer710@gmail.com>
2020-06-09 22:52:22 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,6 @@
  numbers = [1,2,3]
 
+<<<<<<< HEAD
 def square_array(array)
   arr = []
   array.each do |numbers|
@@ -7,5 +8,12 @@ def square_array(array)
 end
 
 return arr
+=======
+def square_array(numbers)
+  arr = []
+ square_array_numbers.each do |numbers|
+  puts "#{numbers}"
+end
+>>>>>>> 7e5c9a7ffcddc45116c0774f855ef8e9923c1d79
 end
 
